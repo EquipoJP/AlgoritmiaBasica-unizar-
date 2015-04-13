1f97508229f37c37f8af28b706e5b62664d454a2
TheRabid <jai_mi_to@hotmail.es>
2015-04-13 16:57:15 +0200

    Actualizado fuerza bruta y grafo.txt

--- Practica2/src/practica2/FuerzaBruta.java
+++ Practica2/src/practica2/FuerzaBruta.java
@@ -1,12 +1,54 @@
 package practica2;
 
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * @author Jaime Ruiz-Borau Vizarraga (546751)
  * @author Patricia Lazaro Tello (554309)
  * 
- * Clase que implementa el algoritmo de fuerza bruta para encontrar el
- * camino hamiltoniano minimo del grafo
+ *         Clase que implementa el algoritmo de fuerza bruta para encontrar el
+ *         camino hamiltoniano minimo del grafo
  */
 
 public class FuerzaBruta {
+
+	public static void main(String[] args) throws FileNotFoundException{
+		String path = "./data/grafo.txt";
+		int[][] matriz = Fichero.getGrafo(path);
+		showCaminosHamiltonianos(matriz);
+	}
+	
+	public static void showCaminosHamiltonianos(int[][] matrizAdyacencia) {
+		List<Integer> visitados = new ArrayList<Integer>();
+
+		searchCaminosHamiltonianosRec(matrizAdyacencia, visitados, 0, 0);
+	}
+
+	private static void searchCaminosHamiltonianosRec(int[][] matrizAdyacencia,
+			List<Integer> visitados, int nodoActual, int nodoOrigen) {
+		// Caso base. Ya hemos encontrado un camino hamiltoniano
+		if (visitados.size() == matrizAdyacencia.length
+				&& nodoActual == nodoOrigen) {
+			visitados.add(nodoActual);
+			System.out.println(visitados.toString());
+		} else if (visitados.size() == matrizAdyacencia.length
+				&& nodoActual != nodoOrigen) {
+			// No hacemos nada. No hemos vuelto al origen, no es un camino
+			// hamiltoniano
+		} else {
+			if (visitados.contains(nodoActual)) {
+				// No hacemos nada. Este nodo ya lo hemos mirado
+			} else {
+				visitados.add(nodoActual);
+				for (int i = 0; i < matrizAdyacencia.length; i++) {
+					if (i != nodoActual) {
+						searchCaminosHamiltonianosRec(matrizAdyacencia,
+								visitados, i, nodoOrigen);
+					}
+				}
+			}
+		}
+	}
 }
