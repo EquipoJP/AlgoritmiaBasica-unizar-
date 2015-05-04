77241cf6fbd64d220736daf027fb70bb570ce3c8
LadyPeanut <plazaro1994@gmail.com>
2015-05-04 15:17:20 +0200

    Comentarios en ProgDinamica

--- Practica2/src/practica2/ProgDinamica.java
+++ Practica2/src/practica2/ProgDinamica.java
@@ -1,5 +1,13 @@
 package practica2;
 
+/**
+ * @author Jaime Ruiz-Borau Vizarraga (546751)
+ * @author Patricia Lazaro Tello (554309)
+ * 
+ *         Clase que implementa el algoritmo de programacion dinamica para
+ *         encontrar el camino hamiltoniano minimo del grafo
+ */
+
 public class ProgDinamica {
 
 	public static void showCaminosHamiltonianos(int[][] matrizAdyacencia) {
